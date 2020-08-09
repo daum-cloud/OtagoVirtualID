d05560a55d31a332372e1f28117c96f65b771456
lucyberes <123>
2020-08-09 15:19:29 +1200

    Data validation and user authentication for login (using username and password)

--- app/src/main/java/com/otago/otagovirtualid/Login.java
+++ app/src/main/java/com/otago/otagovirtualid/Login.java
@@ -1,14 +1,88 @@
 package com.otago.otagovirtualid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    EditText mUsername, mPassword;
+    Button mLoginBtn;
+    FirebaseAuth fAuth;
+   // TextView mForgetPW; Will implement later
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mUsername = findViewById(R.id.txtUsername);
+        mPassword = findViewById(R.id.txtPassword);
+        fAuth = FirebaseAuth.getInstance();
+        mLoginBtn = findViewById(R.id.btnLogin);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                // data validation
+
+                if(TextUtils.isEmpty(username)){
+                    mUsername.setError("Username is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+
+                }
+
+                //authenticate the user - correct username and password to login
+
+                fAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class)); //if login is successful it will take user to main activity
+
+                        }else{
+                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+
+                        }
+
+                    }
+                });
+
+
+
+
+
+            }
+        });
+
+
+
     }
 }
