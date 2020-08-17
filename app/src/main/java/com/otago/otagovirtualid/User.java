0b7eebe6cbd184aa81633050d0b2c996df2a6008
Logan Bartosh <nzlogan@ymail.com>
2020-08-17 22:06:53 +1200

    Attempting to get data from Firebase. Errors persist at this stage.
    Can't seem to get data from Firebase just yet. Only trying with the Name field at this stage - just to confirm we can get it working.
    Have changed the privacy of the database to allow for reading & writing.
    Will look into more tomorrow.

---
+++ app/src/main/java/com/otago/otagovirtualid/User.java
@@ -0,0 +1,156 @@
+package com.otago.otagovirtualid;
+
+public class User {
+    String authID;
+    String email;
+    String enrollingPeriod;
+    String firstName;
+    String lastName;
+    Papers papers;
+    String password;
+    String phone;
+    String studentCode;
+    String studentID;
+    String uRLCurrPhoto;
+    String uRLPrevPhoto;
+    String uRLProofDoc;
+
+    public User(String authID, String email, String enrollingPeriod, String firstName, String lastName, Papers papers, String password, String phone, String studentCode, String studentID, String uRLCurrPhoto, String uRLPrevPhoto, String uRLProofDoc) {
+        this.authID = authID;
+        this.email = email;
+        this.enrollingPeriod = enrollingPeriod;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.papers = papers;
+        this.password = password;
+        this.phone = phone;
+        this.studentCode = studentCode;
+        this.studentID = studentID;
+        this.uRLCurrPhoto = uRLCurrPhoto;
+        this.uRLPrevPhoto = uRLPrevPhoto;
+        this.uRLProofDoc = uRLProofDoc;
+    }
+
+    public String getAuthID() {
+        return authID;
+    }
+
+    public void setAuthID(String authID) {
+        this.authID = authID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEnrollingPeriod() {
+        return enrollingPeriod;
+    }
+
+    public void setEnrollingPeriod(String enrollingPeriod) {
+        this.enrollingPeriod = enrollingPeriod;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Papers getPapers() {
+        return papers;
+    }
+
+    public void setPapers(Papers papers) {
+        this.papers = papers;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getuRLCurrPhoto() {
+        return uRLCurrPhoto;
+    }
+
+    public void setuRLCurrPhoto(String uRLCurrPhoto) {
+        this.uRLCurrPhoto = uRLCurrPhoto;
+    }
+
+    public String getuRLPrevPhoto() {
+        return uRLPrevPhoto;
+    }
+
+    public void setuRLPrevPhoto(String uRLPrevPhoto) {
+        this.uRLPrevPhoto = uRLPrevPhoto;
+    }
+
+    public String getuRLProofDoc() {
+        return uRLProofDoc;
+    }
+
+    public void setuRLProofDoc(String uRLProofDoc) {
+        this.uRLProofDoc = uRLProofDoc;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "authID='" + authID + '\'' +
+                ", email='" + email + '\'' +
+                ", enrollingPeriod='" + enrollingPeriod + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", papers=" + papers +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", studentCode='" + studentCode + '\'' +
+                ", studentID='" + studentID + '\'' +
+                ", uRLCurrPhoto='" + uRLCurrPhoto + '\'' +
+                ", uRLPrevPhoto='" + uRLPrevPhoto + '\'' +
+                ", uRLProofDoc='" + uRLProofDoc + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/otago/otagovirtualid/idTemplateActivity.java
+++ app/src/main/java/com/otago/otagovirtualid/idTemplateActivity.java
@@ -1,21 +1,51 @@
 package com.otago.otagovirtualid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class idTemplateActivity extends AppCompatActivity {
 
-
+    //Get a reference to the User
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("/otago-virtual-id/user");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_id_template);
 
+        User user = null;
 
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(idTemplateActivity.this, "Failed to read database. Contact System Admin", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView txtViewID = findViewById(R.id.txtName);
+
+        if(user != null) {
+            txtViewID.setText(user.getFirstName() + " " + user.getLastName());
+        } else {
+            Toast.makeText(idTemplateActivity.this, "Error with retrieving ID. Contact System Admin", Toast.LENGTH_LONG).show();
+        }
 
 
 
