dd3b605cff4bd52e96904c5d2ee4c5f11d405631
JWill2580 <joshwilson2580@gmail.com>
2020-08-17 21:59:39 +1200

    subject and message now show up with address - formatting/the aesthetic isnt perfect but can fix later

--- app/src/main/java/com/otago/otagovirtualid/FormActivity.java
+++ app/src/main/java/com/otago/otagovirtualid/FormActivity.java
@@ -11,9 +11,9 @@ import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
 
-
-    Button cancelButton, sendButton;
-    EditText subject, msg;
+    public Button cancelButton, sendButton;
+    public EditText subject, msg;
+    public String[] addresses = {"otagovirtualid@gmail.com"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +40,22 @@ public class FormActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:otagovirtualid@gmail.com"));
+                /*Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:otagovirtualid@gmail.com"));
                 intent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString().trim());
                 intent.putExtra(Intent.EXTRA_TEXT, msg.getText().toString().trim());
                 startActivity(intent);
-                System.out.println(subject.getText().toString());
+                System.out.println(subject.getText().toString());*/
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                //intent.setType("message/rfc822");
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, addresses); //For some reason EXTRA_EMAIL only takes string arrays
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+                intent.putExtra(Intent.EXTRA_TEXT, msg.getText().toString());
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                //startActivity(Intent.createChooser(intent, "Send Email"));
             }
         });
     }
