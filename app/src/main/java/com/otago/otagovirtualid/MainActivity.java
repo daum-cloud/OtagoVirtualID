870e4fe293ef7076820373cd1f73cbff13c7796b
Logan Bartosh <nzlogan@ymail.com>
2020-08-17 20:45:07 +1200

    Added authentication checks.
    App checks if the user is logged in before going to the login page if the user selects login.
    
    App checks if the user is logged in before going to the ID page if the user selects it.

--- app/src/main/java/com/otago/otagovirtualid/MainActivity.java
+++ app/src/main/java/com/otago/otagovirtualid/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void idTemplate(View view){
         //Do something in response to button
-        Intent intent = new Intent(this, idTemplateActivity.class);
-        startActivity(intent);
+
+        //Checking that the user is authenticated
+        fAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(this, idTemplateActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this, "ERROR! Please sign in first " ,Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
--- app/src/main/java/com/otago/otagovirtualid/idTemplateActivity.java
+++ app/src/main/java/com/otago/otagovirtualid/idTemplateActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+
 public class idTemplateActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_id_template);
+
+
+
+
+
+
     }
 }
