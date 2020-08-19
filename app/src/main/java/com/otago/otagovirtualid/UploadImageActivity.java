dfae5e08a539898cec9246070ffe2c6fd038e864
JWill2580 <joshwilson2580@gmail.com>
2020-08-19 21:57:36 +1200

    adding image upload and camera features. Need to do firebase stuff

--- app/src/main/java/com/otago/otagovirtualid/UploadImageActivity.java
+++ app/src/main/java/com/otago/otagovirtualid/UploadImageActivity.java
@@ -8,22 +8,34 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -31,9 +43,11 @@ public class UploadImageActivity extends AppCompatActivity {
 
     public static final int CAMERA_PERMISSION_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 103;
     ImageView selectedImg;
-    Button galleryBtn, cameraBtn;
+    Button galleryBtn, cameraBtn, submitBtn;
     String currentPhotoPath;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,9 @@ public class UploadImageActivity extends AppCompatActivity {
         selectedImg = findViewById(R.id.selectedImage);
         galleryBtn = findViewById(R.id.galleryBtn);
         cameraBtn = findViewById(R.id.cameraBtn);
+        submitBtn = findViewById(R.id.submitBtn);
+
+        storageRef = FirebaseStorage.getInstance().getReference();
 
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,17 +72,32 @@ public class UploadImageActivity extends AppCompatActivity {
         galleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(UploadImageActivity.this, "Gallery button responding", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(UploadImageActivity.this, "Gallery button responding", Toast.LENGTH_SHORT).show();
+                //Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                //startActivityForResult(gallery, GALLERY_REQUEST_CODE);
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, GALLERY_REQUEST_CODE);
+            }
+        });
+
+        submitBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
     }
+
+   
 
     private void askCameraPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE); //Sends out a code
         } else {
             dispatchTakePictureIntent();
+            //openCamera();
         }
     }
 
@@ -82,28 +114,72 @@ public class UploadImageActivity extends AppCompatActivity {
         }
     }
 
-    /*private void openCamera() {
+
+    //private void openCamera() {
         //Toast.makeText(this, "Camera ready to go", Toast.LENGTH_SHORT).show();
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera, CAMERA_REQUEST_CODE);
-    }*/
+      //  Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       // startActivityForResult(camera, CAMERA_REQUEST_CODE);
+    //}
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == CAMERA_REQUEST_CODE) {
            /* Bitmap image = (Bitmap) data.getExtras().get("data");
             selectedImg.setImageBitmap(image);
             */
-           if(resultCode == Activity.RESULT_OK) {
+            if (resultCode == RESULT_OK) {
                 File f = new File(currentPhotoPath);
                 selectedImg.setImageURI(Uri.fromFile(f));
-                Log.d("tag", "Absolute URL = " + Uri.fromFile(f));
-           }
+                submitBtn.setVisibility(View.VISIBLE);
+                //Toast.makeText(this, "Trello", Toast.LENGTH_SHORT).show();
+                //Bitmap image = (Bitmap) data.getExtras().get("data");
+                //selectedImg.setImageBitmap(image);
+                //Log.d("tag", "Absolute URL = " + Uri.fromFile(f));
+
+                //Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                //File f = new File(currentPhotoPath);
+                //Uri contentUri = Uri.fromFile(f);
+                //mediaScanIntent.setData(contentUri);
+                //this.sendBroadcast(mediaScanIntent);
+            }
         }
+        /*if (requestCode == GALLERY_REQUEST_CODE) {
+           if(requestCode == RESULT_OK){
+               Uri contentUri = data.getData();
+               String timeStamp =  new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+               String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
+               Log.d("tag", "Absolute URL for gallery image = " + imageFileName);
+               selectedImg.setImageURI(contentUri);
+           }*/
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                   // Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                    selectedImg.setImageBitmap(selectedImage);
+                    submitBtn.setVisibility(View.VISIBLE);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(UploadImageActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+
+            } else {
+                Toast.makeText(UploadImageActivity.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 
-
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
 
 
     private File createImageFile() throws IOException {
@@ -111,6 +187,7 @@ public class UploadImageActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); //Could be issue with type imported
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -121,8 +198,6 @@ public class UploadImageActivity extends AppCompatActivity {
         currentPhotoPath = image.getAbsolutePath();
         return image;
     }
-
-    //static final int REQUEST_TAKE_PHOTO = 1;
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
