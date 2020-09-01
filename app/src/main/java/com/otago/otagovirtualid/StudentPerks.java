e38df9fa9eb8648307fa19c151d42b58b3f5bd16
matma236 <matma236@student.otago.ac.nz>
2020-09-01 19:33:18 +1200

    Perks adapter needs data from firebase and few more implementation methods to be completed

--- app/src/main/java/com/otago/otagovirtualid/StudentPerks.java
+++ app/src/main/java/com/otago/otagovirtualid/StudentPerks.java
@@ -1,27 +1,33 @@
 package com.otago.otagovirtualid;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toolbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.otago.otagovirtualid.databinding.ActivityStudentPerksBinding;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class StudentPerks extends AppCompatActivity {
 
-    private ActivityStudentPerksBinding binding;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Intent intent = getIntent();
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_student_perks);
+        setContentView(R.layout.activity_student_perks);
+        recyclerView = (RecyclerView) findViewById(R.id.recView1);
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_student_perks);
-        setSupportActionBar(binding.toolbar);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new PerksAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 
 }
