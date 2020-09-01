6a028b2aab524126a2f7bd7e7b69018ea62800e3
matma236 <matma236@student.otago.ac.nz>
2020-09-01 13:21:54 +1200

    Perks info layout completed. Data addition yet to be implemented.

--- app/src/main/java/com/otago/otagovirtualid/StudentPerks.java
+++ app/src/main/java/com/otago/otagovirtualid/StudentPerks.java
@@ -3,16 +3,25 @@ package com.otago.otagovirtualid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.otago.otagovirtualid.databinding.ActivityStudentPerksBinding;
 
 public class StudentPerks extends AppCompatActivity {
+
+    private ActivityStudentPerksBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_perks);
+        //setContentView(R.layout.activity_student_perks);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_student_perks);
+        setSupportActionBar(binding.toolbar);
     }
 
 }
