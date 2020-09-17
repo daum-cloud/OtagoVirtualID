fd2d33eea632eab55ebb6fb7062e7b5d3b84f1b2
matma236 <matma236@student.otago.ac.nz>
2020-09-17 15:30:11 +1200

    App launch and layout changes. Login class removed and fitted within MainActivity. Main page created as "home Screen/Activity Main"

--- app/src/main/java/com/otago/otagovirtualid/MainActivity.java
+++ app/src/main/java/com/otago/otagovirtualid/MainActivity.java
@@ -1,73 +1,140 @@
 package com.otago.otagovirtualid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mUsername, mPassword;
+    Button mLoginBtn;
     FirebaseAuth fAuth;
+    TextView mForgetPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    //Called when user clicks login
-    public void loginToApp(View view) {
-        // Do something in response to button
-
-        //Checking that the user is authenticated before allowing them to login
-        fAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            Toast.makeText(MainActivity.this, "You are already signed in, go to ID" ,Toast.LENGTH_LONG).show();
-        } else {
-            Intent intent = new Intent(this, Login.class);
-            startActivity(intent);
+        try
+        {
+            this.getSupportActionBar().hide();
         }
-    }
+        catch (NullPointerException e){}
 
-    //Called when user clicks askOtago
-    public void askOtago(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, AskOtagoActivity.class);
-        startActivity(intent);
-    }
+        setContentView(R.layout.activity_login);
 
-    public void idTemplate(View view){
-        //Do something in response to button
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
 
-        //Checking that the user is authenticated
+        mUsername = findViewById(R.id.txtUsername);
+        mPassword = findViewById(R.id.txtPassword);
         fAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            Intent intent = new Intent(this, idTemplateActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(MainActivity.this, "Please sign in first" ,Toast.LENGTH_LONG).show();
-        }
+        mLoginBtn = findViewById(R.id.btnLogin);
+        mForgetPW = findViewById(R.id.txtForgotPW);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                // data validation
+
+                if (TextUtils.isEmpty(username)) {
+                    mUsername.setError("Username is Required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+
+                }
+
+                //authenticate the user - correct username and password to login
+
+                fAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainPage.class)); //if login is successful it will take user to main activity
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
 
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+        mForgetPW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter Your Email to Receive Reset Link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Extract the email and send reset link
+
+                        String mail = resetMail.getText().toString();
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Reset Link Sent To Your Email", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Error! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close the dialog
+
+                    }
+                });
+                passwordResetDialog.create().show();
+            }
+        });
     }
 
-    //Called when user clicks perks button
-    public void perkButton(View view) {
-        Intent intent = new Intent(this, PerkListActivity.class);
-        startActivity(intent);
-    }
-
-
-    //Called when user clicks Upload Image
-    public void uploadImage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, UploadImageActivity.class);
-        startActivity(intent);
-    }
 }
---
+++ app/src/main/java/com/otago/otagovirtualid/MainPage.java
@@ -0,0 +1,57 @@
+package com.otago.otagovirtualid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainPage extends AppCompatActivity {
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Called when user clicks askOtago
+    public void askOtago(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, AskOtagoActivity.class);
+        startActivity(intent);
+    }
+
+    public void idTemplate(View view){
+        //Do something in response to button
+
+        //Checking that the user is authenticated
+        fAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(this, idTemplateActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainPage.this, "Please sign in first" ,Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Called when user clicks perks button
+    public void perkButton(View view) {
+        Intent intent = new Intent(this, PerkListActivity.class);
+        startActivity(intent);
+    }
+
+
+    //Called when user clicks Upload Image
+    public void uploadImage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, UploadImageActivity.class);
+        startActivity(intent);
+    }
+
+}
