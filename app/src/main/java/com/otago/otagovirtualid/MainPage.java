f4f9a710a7d04460ac4b403117414a94861a234d
lucyberes <berlu021@student.otago.ac.nz>
2020-09-18 14:44:00 +1200

    Added bottom navigation bar to the main page.

--- app/src/main/java/com/otago/otagovirtualid/MainPage.java
+++ app/src/main/java/com/otago/otagovirtualid/MainPage.java
@@ -2,21 +2,32 @@ package com.otago.otagovirtualid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     FirebaseAuth fAuth;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.navigation_id);
+
     }
 
     //Called when user clicks askOtago
@@ -46,7 +57,6 @@ public class MainPage extends AppCompatActivity {
         startActivity(intent);
     }
 
-
     //Called when user clicks Upload Image
     public void uploadImage(View view) {
         // Do something in response to button
@@ -58,4 +68,35 @@ public class MainPage extends AppCompatActivity {
     public void onBackPressed() {
     }
 
+
+    //Navigation
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+
+        //Takes user to upload image activity
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_uploadImage:
+                Intent intent = new Intent(this, UploadImageActivity.class);
+                startActivity(intent);
+        }
+
+        //Takes user to perks activity
+
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_perks:
+                Intent intent = new Intent(this, PerkListActivity.class);
+                startActivity(intent);
+
+        }
+
+        //Takes user to askOtago activity
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_help:
+                Intent intent = new Intent(this, AskOtagoActivity.class);
+                startActivity(intent);
+
+        }
+        return false;
+    }
 }
