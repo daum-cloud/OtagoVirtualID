1a6b119f8e1e084629f24be72e043d675e2f89f8
Logan Bartosh <nzlogan@ymail.com>
2020-10-06 17:32:52 +1300

    Required to change the verified field to "ready" so we can pull only users who have sumbitted a photo in the web app.

--- app/src/main/java/com/otago/otagovirtualid/idTemplateActivity.java
+++ app/src/main/java/com/otago/otagovirtualid/idTemplateActivity.java
@@ -137,7 +137,7 @@ public class idTemplateActivity extends AppCompatActivity {
                 //If ID is verified, show their ID
                 if (snapshot.child("verified").getValue(String.class).equals("verified")) {
                     Glide.with(idTemplateActivity.this).load(snapshot.child("uRLCurrPhoto").getValue()).into(imageIDPhoto);
-                //Else - e.g. ID is not verified, display the following
+                    //Else - e.g. ID is not verified, display the following
                 } else {
                     Glide.with(idTemplateActivity.this).load("https://firebasestorage.googleapis.com/v0/b/otago-virtual-id.appspot.com/o/systemImages%2FerrorImage.png?alt=media&token=2e86b7a6-5aef-4324-b995-6a9f7ab00e6e").into(imageIDPhoto);
                 }
